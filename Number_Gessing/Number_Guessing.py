89d1f6cb95654354acbb2fa9261d6bdb65d917a3
Biney Jotham <126997039+bineyjotham@users.noreply.github.com>
2024-05-19 08:22:14 -0700

    Add files via upload

---
+++ Number_Gessing/Number_Guessing.py
@@ -0,0 +1,45 @@
+import random
+
+# randrange excludes the upper limit
+# r = random.randrange(-5, 11)
+# print(r)
+# randint includes hte upper limit
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number that is bigger than 0 next time!")
+        quit()
+
+r = random.randrange(0, top_of_range)
+print(r)
+guesses = 0
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+
+    else:
+        print("Please type a number next time!")
+        continue
+
+    if user_guess == r:
+        print("You got it correct!")
+        break
+    elif user_guess < r:
+        print("You got it wrong!")
+        print("The number is greater than your input!")
+    else:
+        print("You got it wrong!")
+        print("The number is less than your input!")
+
+print("You got it in", guesses, "guesses")
+
+# print("You got it in " + str(guesses) + " guesses")
+# randint includes the upper limit
+# a = random.randint(-2, 11)
+# print(a)
